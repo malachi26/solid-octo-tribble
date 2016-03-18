309ffb9b4276bd575966adc496d0c431b1cbb88a
Lyle Hart III <lyle.hart@cellularsales.com>
2016-03-18 14:55:24 -0400

    work commit
    I am not sure what I am doing here, I can't get my controllers to work.... TGIF!

--- AngularPlayGround/Assets/app/controllers/homeCtrl.js
+++ AngularPlayGround/Assets/app/controllers/homeCtrl.js
@@ -1,5 +1,24 @@
 ﻿angular.module('home', [])
     .controller('homeCtrl',['$scope','$http', function ($scope, $http) {
+        var vm = $scope;
+        vm.k = [0, 1, 2, 3, 4, 5, 6, 7];
+
+        vm.phones = [
+            {
+                'name': 'Nexus S',
+                'snippet': 'Fast just got faster with Nexus S.'
+            },
+            {
+                'name': 'Motorola XOOM™ with Wi-Fi',
+                'snippet': 'The Next, Next Generation tablet.'
+            },
+            {
+                'name': 'MOTOROLA XOOM™',
+                'snippet': 'The Next, Next Generation tablet.'
+            }
+        ];
+
+
         $scope.alert = function () {
             alert("WOW");
         }
